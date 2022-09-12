aad78dbfeb2fb9467c9528606a956d199184e591
milovangudelj <milovan.gudelj@gmail.com>
2022-09-12 14:21:57 +0200

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+	return (
+		<div>
+			<main>
+				<h1>Welcome to my new website</h1>
+			</main>
+		</div>
+	);
+};
+
+export default Home;
