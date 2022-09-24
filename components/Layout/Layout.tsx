0a3382112b0367dcfb360b058a60c0849bc9734b
milovangudelj <milovan.gudelj@gmail.com>
2022-09-24 09:08:46 +0200

    Fixed element height issue

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -10,7 +10,7 @@ export const Layout = ({ children, ...props }: LayoutProps) => {
 
 	return (
 		<div
-			className={`absolute inset-0 ${
+			className={`h-screen w-screen ${
 				scrollable ? "overflow-y-scroll" : "overflow-hidden"
 			} bg-black text-white`}
 			{...props}
