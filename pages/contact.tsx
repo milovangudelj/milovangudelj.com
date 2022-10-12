0a3af71deef3d9dcb9627252aba7ebcc736d633d
milovangudelj <milovan.gudelj@gmail.com>
2022-10-12 17:39:49 +0200

    Added section

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,4 +1,10 @@
+import Image from "next/future/image";
+
 import { HeadMeta, Layout } from "../components";
+
+import igLogo from "../public/images/igLogo.png";
+import twLogo from "../public/images/twLogo.svg";
+import drLogo from "../public/images/drLogo.svg";
 
 const meta = {
 	title: "Milovan Gudelj - Contact me",
@@ -12,12 +18,76 @@ const Contact = () => {
 	return (
 		<Layout>
 			<HeadMeta metadata={meta} />
+			<section className="scroll-mt-[72px] md:scroll-mt-[88px]">
+				<main className="max-w-8xl mx-auto py-16 px-8 md:py-32 md:px-16 flex justify-between items-center">
+					<div className="space-y-28">
+						<div className="space-y-8">
+							<h1 className="text-h3-mobile md:text-h2-mobile xl:text-h2">
+								My @s
+							</h1>
+							<p className="text-body-lg xl:max-w-[680px]">
+								You can find me pretty much anywhere by typing in my
+								name. But just in case here are a few links to my social
+								platforms.
+							</p>
+						</div>
+						<div className="flex space-x-12 py-0.5 items-center">
+							<a
+								href="https://instagram.com/milovangudelj"
+								target="_blank"
+								rel="noreferrer"
+								className="hover:text-[#E4405F] transition text-body-lg md:text-sub-heading-mobile xl:text-sub-heading font-semibold"
+							>
+								Instagram ↗
+							</a>
+							<a
+								href="https://dribbble.com/milovangudelj"
+								target="_blank"
+								rel="noreferrer"
+								className="hover:text-[#EA4C89] transition text-body-lg md:text-sub-heading-mobile xl:text-sub-heading font-semibold"
+							>
+								Dribbble ↗
+							</a>
+							<a
+								href="https://twitter.com/milovangudelj"
+								target="_blank"
+								rel="noreferrer"
+								className="hover:text-[#1DA1F2] transition text-body-lg md:text-sub-heading-mobile xl:text-sub-heading font-semibold"
+							>
+								Twitter ↗
+							</a>
+						</div>
+					</div>
+					<div className="space-y-20 rotate-12 origin-center flex flex-col items-center">
+						<Image
+							alt="Dribbble logo"
+							className="-rotate-12 origin-center"
+							src={drLogo}
+							width={100}
+						/>
+						<div className="flex space-x-[100px]">
+							<Image
+								alt="Twitter logo"
+								className="-rotate-12 origin-center"
+								src={twLogo}
+								width={100}
+							/>
+							<Image
+								alt="Instagram logo"
+								className="-rotate-12 origin-center"
+								src={igLogo}
+								width={100}
+							/>
+						</div>
+					</div>
+				</main>
+			</section>
 			<section className="bg-blue scroll-mt-[72px] md:scroll-mt-[88px]">
-				<main className="max-w-8xl mx-auto py-16 px-8 md:py-32 md:px-16 space-y-16">
+				<div className="max-w-8xl mx-auto py-16 px-8 md:py-32 md:px-16 space-y-16">
 					<div className="space-y-8">
-						<h1 className="text-h3-mobile md:text-h2-mobile xl:text-h2">
+						<h2 className="text-h3-mobile md:text-h2-mobile xl:text-h2">
 							Let&apos;s work together
-						</h1>
+						</h2>
 						<p className="text-body-lg xl:max-w-[680px]">
 							If you like what I do please feel free to contact me and
 							who knows, we might build the next big thing or just a cool
@@ -48,7 +118,7 @@ const Contact = () => {
 							</svg>
 						</span>
 					</div>
-				</main>
+				</div>
 			</section>
 		</Layout>
 	);
