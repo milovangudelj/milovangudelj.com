0fcffaaeccb5f1dca50769f48c47c7c8f13ba0e3
milovangudelj <milovan.gudelj@gmail.com>
2022-11-22 01:42:45 +0100

    Fixed type error

--- components/NavLinks/NavLinks.tsx
+++ components/NavLinks/NavLinks.tsx
@@ -22,27 +22,28 @@ const links = [
 	},
 ];
 
-export const NavLinks = forwardRef<HTMLElement>(
-	({ className, ...props }: ComponentPropsWithoutRef<"nav">, ref) => {
-		return (
-			<nav className={className} {...props} ref={ref}>
-				<ul className="md:flex">
-					{links.map((link) => (
-						<li
-							key={link.key}
-							className="border-t-2 border-black bg-black last:border-b-2 md:border-t-0 md:border-l-2 md:last:border-b-0 md:last:border-r-0 xl:last:border-r-2"
+export const NavLinks = forwardRef<
+	HTMLElement,
+	ComponentPropsWithoutRef<"nav">
+>(({ className, ...props }, ref) => {
+	return (
+		<nav className={className} {...props} ref={ref}>
+			<ul className="md:flex">
+				{links.map((link) => (
+					<li
+						key={link.key}
+						className="border-t-2 border-black bg-black last:border-b-2 md:border-t-0 md:border-l-2 md:last:border-b-0 md:last:border-r-0 xl:last:border-r-2"
+					>
+						<Link
+							href={link.href}
+							className={`${link.color} inline-block w-full px-8 py-6 text-sub-heading text-white transition-all will-change-transform hover:-translate-x-1 hover:-translate-y-1 md:w-max`}
 						>
-							<Link
-								href={link.href}
-								className={`${link.color} inline-block w-full px-8 py-6 text-sub-heading text-white transition-all will-change-transform hover:-translate-x-1 hover:-translate-y-1 md:w-max`}
-							>
-								{link.label}
-							</Link>
-						</li>
-					))}
-				</ul>
-			</nav>
-		);
-	}
-);
+							{link.label}
+						</Link>
+					</li>
+				))}
+			</ul>
+		</nav>
+	);
+});
 NavLinks.displayName = "NavLinks";
