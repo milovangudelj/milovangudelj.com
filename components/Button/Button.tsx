df5aa86fd94d1d846afe3acedfa5071b40aa4382
milovangudelj <milovan.gudelj@gmail.com>
2022-11-28 18:18:44 +0100

    Using `cva` for style variants

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,20 +1,43 @@
-import { ComponentProps } from "react";
 import { cva, type VariantProps } from "cva";
-import { twMerge } from "tailwind-merge";
+
+const button = cva(
+	"border-2 transition-all will-change-[filter] hover:drop-shadow-brutal",
+	{
+		variants: {
+			intent: {
+				primary: "bg-salmon",
+				secondary: "bg-lavender",
+				error: "bg-orange",
+			},
+			size: {
+				sm: "py-1 px-2 text-button-sm",
+				md: "py-1.5 px-3 text-button-md",
+				lg: "py-2 px-4 text-button",
+			},
+		},
+		compoundVariants: [
+			{ intent: "primary", size: "sm", className: "uppercase" },
+		],
+		defaultVariants: {
+			intent: "primary",
+			size: "lg",
+		},
+	}
+);
+
+export interface ButtonProps
+	extends React.HTMLAttributes<HTMLButtonElement>,
+		VariantProps<typeof button> {}
 
 export const Button = ({
-	className,
 	children,
+	intent,
+	size,
+	className,
 	...props
-}: ComponentProps<"button">) => {
+}: ButtonProps) => {
 	return (
-		<button
-			className={twMerge(
-				"border-2 bg-salmon py-2 px-4 text-button transition-all will-change-[filter] hover:drop-shadow-brutal",
-				className
-			)}
-			{...props}
-		>
+		<button className={button({ intent, size, className })} {...props}>
 			{children}
 		</button>
 	);
