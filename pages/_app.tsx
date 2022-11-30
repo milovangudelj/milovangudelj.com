2a9672bb0b87cca4c2019b9287e081281b8441c0
milovangudelj <milovan.gudelj@gmail.com>
2022-11-30 01:13:51 +0100

    Removed `@next/font` for the time being

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,9 @@
-import { Inter, Space_Grotesk } from "@next/font/google";
 import type { AppProps } from "next/app";
 
 import "../styles/globals.css";
 
 import { ScrollProvider } from "../lib/scrollContext";
 import { WindowSizeProvider } from "../lib/windowSizeContext";
-
-const inter = Inter({
-	variable: "--font-inter",
-	subsets: ["latin-ext"],
-});
-const spaceGrotesk = Space_Grotesk({
-	variable: "--font-space-grotesk",
-	subsets: ["latin-ext"],
-});
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
@@ -27,9 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 						}
 					`}
 				</style>
-				<div
-					className={`${inter.variable} ${spaceGrotesk.variable} h-fill font-sans`}
-				>
+				<div className={`h-fill font-sans`}>
 					<Component {...pageProps} />
 				</div>
 			</ScrollProvider>
