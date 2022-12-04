931dfd19b604aff68815d538e870d49db7605666
milovangudelj <milovan.gudelj@gmail.com>
2022-12-04 02:32:32 +0100

    Added genre solar system

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,5 @@
 import Image from "next/legacy/image";
+import { useEffect } from "react";
 
 import {
 	Artists,
@@ -19,7 +20,51 @@ const meta = {
 	image: "https://milovangudelj.com/images/og-image.png",
 };
 
+const topGenres = [
+	{
+		id: "tg_01",
+		label: "Rap",
+		color: "bg-spotify-brand",
+	},
+	{
+		id: "tg_02",
+		label: "Pop",
+		color: "bg-spotify-orange",
+	},
+	{
+		id: "tg_03",
+		label: "Italian hip hop",
+		color: "bg-spotify-pink",
+	},
+	{
+		id: "tg_04",
+		label: "Modern Rock",
+		color: "bg-spotify-yellow",
+	},
+	{
+		id: "tg_05",
+		label: "Alternative R&B",
+		color: "bg-spotify-purple",
+	},
+];
+
 const About = () => {
+	useEffect(() => {
+		const r = 656 / 2;
+		const a = Math.PI / 8;
+		const offsetA = (5 * Math.PI) / 8;
+
+		for (let i = 1; i <= 5; i++) {
+			const angle = 2 * Math.PI - (offsetA + a * i);
+			const x = r * Math.cos(angle);
+			const y = r * Math.sin(angle);
+			console.log({ a: ((offsetA + a * i) * 180) / Math.PI });
+
+			document.documentElement.style.setProperty(`--planet${i}-x`, `${x}px`);
+			document.documentElement.style.setProperty(`--planet${i}-y`, `${y}px`);
+		}
+	}, []);
+
 	return (
 		<Layout>
 			<HeadMeta metadata={meta} />
@@ -41,8 +86,35 @@ const About = () => {
 							little bit of information about that.
 						</p>
 					</div>
-					<NowPlaying />
-					<Artists />
+					<div className="relative">
+						<NowPlaying />
+						<Artists className="mt-16" />
+						<div className="absolute left-full top-1/2 -translate-x-1/2 -translate-y-1/2">
+							<div className="-mr-16 flex h-[656px] w-[656px] items-center justify-center rounded-full border-2 border-black/20 md:-mr-32">
+								<div className="flex h-[496px] w-[496px] items-center justify-center rounded-full border-2 border-black bg-spotify-orange">
+									<h3 className="mr-8 max-w-[12ch] -translate-x-1/2 text-right text-sub-heading-mobile text-black">
+										My genres' solar system
+									</h3>
+								</div>
+								{topGenres.map((genre, idx) => (
+									<div
+										key={genre.id}
+										className={`absolute h-8 w-8 rounded-full border-2 border-black ${genre.color}`}
+										style={{
+											translate: `var(--planet${
+												idx + 1
+											}-x) var(--planet${idx + 1}-y)`,
+										}}
+									>
+										<span className="absolute right-full top-1/2 mr-6 inline-block w-max -translate-y-1/2 text-sub-heading-mobile">
+											<span className="text-salmon">{idx + 1}.</span>{" "}
+											{genre.label}
+										</span>
+									</div>
+								))}
+							</div>
+						</div>
+					</div>
 				</Container>
 			</Section>
 		</Layout>
