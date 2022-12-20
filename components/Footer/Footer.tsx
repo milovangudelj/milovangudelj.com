198bd5a159fffa4f0d654d0c001cd4270611acf7
milovangudelj <milovan.gudelj@gmail.com>
2022-12-20 03:04:07 +0100

    Better sizing even at smaller screens

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,14 +3,14 @@ export const Footer = () => {
 		<footer className="fixed bottom-0 w-full">
 			<div className="mx-auto flex w-full max-w-[1440px] items-center justify-between p-4 text-label-md md:px-16">
 				<span>Milovan Gudelj &copy; 2022</span>
-				<ul className="flex space-x-2 md:space-x-4">
+				<ul className="flex space-x-4">
 					<li>
 						<a
 							target="_blank"
 							rel="noreferrer"
 							href="https://dribbble.com/milovangudelj"
 						>
-							Dribbble
+							Dr
 						</a>
 					</li>
 					<li>
@@ -19,7 +19,7 @@ export const Footer = () => {
 							rel="noreferrer"
 							href="https://twitter.com/milovangudelj"
 						>
-							Twitter
+							Tw
 						</a>
 					</li>
 					<li>
@@ -28,7 +28,7 @@ export const Footer = () => {
 							rel="noreferrer"
 							href="https://instagram.com/milovangudelj"
 						>
-							Instagram
+							Ig
 						</a>
 					</li>
 				</ul>
