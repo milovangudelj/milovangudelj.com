0bf6b999569c7f6f18136adb57299e76295d79f7
milovangudelj <milovan.gudelj@gmail.com>
2023-02-01 22:18:07 +0100

    Added experimantal app dir

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	experimental: {
+		appDir: true,
+	},
 	swcMinify: true,
 	images: {
 		domains: ["milovangudelj.com", "media.graphassets.com", "i.scdn.co"],
