11dd16fb101480085741809cb65149c14804ec9b
milovangudelj <milovan.gudelj@gmail.com>
2023-03-27 22:33:15 +0200

    Added alternate lang links to sitemap

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -10,40 +10,52 @@ const GET_SLUGS = gql`
 	}
 `;
 
-const CASE_STUDIES_BASE_URL = "https://www.milovangudelj.com/work";
-
 function generateSiteMap(caseStudies: string[]) {
 	return `<?xml version="1.0" encoding="UTF-8"?>
-   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <url>
-       <loc>https://www.milovangudelj.com</loc>
-     </url>
-     <url>
-       <loc>https://www.milovangudelj.com/about</loc>
-     </url>
-     <url>
-       <loc>https://www.milovangudelj.com/contact</loc>
-     </url>
-     <url>
-       <loc>https://www.milovangudelj.com/login</loc>
-     </url>
-     <url>
-       <loc>https://www.milovangudelj.com/music-stats</loc>
-     </url>
-     <url>
-       <loc>https://www.milovangudelj.com/work</loc>
-     </url>
-     ${caseStudies
+	<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+		<url>
+			<loc>https://www.milovangudelj.com</loc>
+			<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com"/>
+			<xhtml:link rel="alternate" hreflang="it" href="https://www.milovangudelj.com/it"/>
+		</url>
+		<url>
+			<loc>https://www.milovangudelj.com/about</loc>
+			<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/about"/>
+			<xhtml:link rel="alternate" hreflang="it" href="https://www.milovangudelj.com/it/about"/>
+		</url>
+		<url>
+			<loc>https://www.milovangudelj.com/contact</loc>
+			<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/contact"/>
+			<xhtml:link rel="alternate" hreflang="it" href="https://www.milovangudelj.com/it/contact"/>
+		</url>
+		<url>
+			<loc>https://www.milovangudelj.com/login</loc>
+			<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/login"/>
+			<xhtml:link rel="alternate" hreflang="it" href="https://www.milovangudelj.com/it/login"/>
+		</url>
+		<url>
+			<loc>https://www.milovangudelj.com/music-stats</loc>
+			<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/music-stats"/>
+			<xhtml:link rel="alternate" hreflang="it" href="https://www.milovangudelj.com/it/music-stats"/>
+		</url>
+		<url>
+			<loc>https://www.milovangudelj.com/work</loc>
+			<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/work"/>
+			<xhtml:link rel="alternate" hreflang="it" href="https://www.milovangudelj.com/it/work"/>
+		</url>
+		${caseStudies
 			.map((slug) => {
 				return `
-       <url>
-           <loc>${`${CASE_STUDIES_BASE_URL}/${slug}`}</loc>
-       </url>
-     `;
+				<url>
+					<loc>${`https://www.milovangudelj.com/work/${slug}`}</loc>
+					<xhtml:link rel="alternate" hreflang="en" href="${`https://www.milovangudelj.com/work/${slug}`}"/>
+					<xhtml:link rel="alternate" hreflang="it" href="${`https://www.milovangudelj.com/it/work/${slug}`}"/>
+				</url>
+				`;
 			})
 			.join("")}
-   </urlset>
- `;
+	</urlset>
+	`;
 }
 
 const SiteMap = () => null;
