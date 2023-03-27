584fa0a19eeea0ab48b2582e1cc07aa805ca2c3b
milovangudelj <milovan.gudelj@gmail.com>
2023-03-28 00:20:27 +0200

    Fixwed errors introduced when moving stuff around

--- components/ProjectShowcase/ProjectShowcase.tsx
+++ components/ProjectShowcase/ProjectShowcase.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/legacy/image";
 
-import { Project } from "../../app/(pages)/work/page";
+import { Project } from "../../app/[locale]/work/page";
 import { Link } from "next-intl";
 import { RichText } from "@graphcms/rich-text-react-renderer";
 import { Button } from "../Button";
