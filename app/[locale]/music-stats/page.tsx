46876ca09c8bc6c4a3232262d913b69766a99613
milovangudelj <milovan.gudelj@gmail.com>
2023-04-22 12:01:36 +0200

    Moving around imports

--- app/[locale]/music-stats/page.tsx
+++ app/[locale]/music-stats/page.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { getTranslations } from "next-intl/server";
+
+import { Artist, Track } from "@lib/types";
+import { getPalette, Palette } from "@utils/getPalette";
 
 import { BigAssStar } from "@components/BigAssStar";
 import { Container } from "@components/Container";
 import { Section } from "@components/Section";
-import { getPalette, Palette } from "@utils/getPalette";
-import { Artist, Track } from "@lib/types";
+
 import { FormData } from "./ControlsBar";
 import { StatsSection } from "./StatsSection";
-import { getTranslations } from "next-intl/server";
 
 export interface UserStats {
 	stats: {
