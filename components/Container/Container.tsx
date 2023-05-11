9dd01b6c9901627463525557330368e4f7ca8189
milovangudelj <milovan.gudelj@gmail.com>
2023-05-11 19:35:56 +0200

    Padding adjustments

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -9,7 +9,12 @@ export const Container = ({
 	className?: string;
 }) => {
 	return (
-		<div className={twMerge("mx-auto max-w-7xl space-y-16", className)}>
+		<div
+			className={twMerge(
+				"mx-auto max-w-7xl space-y-16 px-8 2xl:px-0",
+				className
+			)}
+		>
 			{children}
 		</div>
 	);
