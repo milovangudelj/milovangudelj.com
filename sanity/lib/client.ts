b50ddbe4621dcbfcba3ff0e440770d23aaa4feae
milovangudelj <milovan.gudelj@gmail.com>
2023-05-29 23:41:14 +0200

    Exporting queries from `queries.ts`

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,25 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId, useCdn } from '../env'
+import { apiVersion, dataset, projectId, useCdn } from "@/sanity/env";
+import { ProjectPayload } from "@/sanity/types";
+
+import { projectBySlugQuery, projectPaths } from "./queries";
 
 export const client = createClient({
-  apiVersion,
-  dataset,
-  projectId,
-  useCdn,
-})
+	apiVersion,
+	dataset,
+	projectId,
+	useCdn,
+});
+
+export async function getProjectBySlug({
+	slug,
+}: {
+	slug: string;
+}): Promise<ProjectPayload | undefined> {
+	return await client.fetch(projectBySlugQuery, { slug });
+}
+
+export async function getProjectPaths(): Promise<string[]> {
+	return (await client.fetch(projectPaths)) || [];
+}
---
+++ sanity/types/index.ts
@@ -0,0 +1,26 @@
+import type { PortableTextBlock } from "@portabletext/types";
+import type { Image } from "sanity";
+
+export interface ShowcaseProject {
+	_type: string;
+	coverImage?: Image;
+	overview?: PortableTextBlock[];
+	slug?: string;
+	tags?: string[];
+	title?: string;
+}
+
+export interface ProjectPayload {
+	client?: string;
+	coverImage?: Image;
+	description?: PortableTextBlock[];
+	duration?: {
+		start?: string;
+		end?: string;
+	};
+	overview?: PortableTextBlock[];
+	site?: string;
+	slug: string;
+	tags?: string[];
+	title?: string;
+}
