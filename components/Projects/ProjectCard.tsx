34397f11baff7603f7b5cd3fd7c6dc91fade69ba
milovangudelj <milovan.gudelj@gmail.com>
2023-06-17 15:31:48 +0200

    Testing performance difference

--- components/Projects/ProjectCard.tsx
+++ components/Projects/ProjectCard.tsx
@@ -18,11 +18,7 @@ export const ProjectCard = ({ title, site, cover }: ProjectCardProps) => {
 				className="relative inline-block aspect-video h-[150px] cursor-pointer overflow-hidden rounded-2xl md:h-[300px]"
 			>
 				<Image
-					src={urlForImage(cover.image)
-						.width(Math.floor((600 * 16) / 9))
-						.height(600)
-						.quality(100)
-						.url()}
+					src={urlForImage(cover.image).url()}
 					alt={cover.image.alt ?? cover.image.caption ?? title}
 					title={cover.image.alt ?? cover.image.caption ?? title}
 					width={Math.floor((300 * 16) / 9)}
