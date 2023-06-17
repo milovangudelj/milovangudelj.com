b842bbcbe819476fde616891b85406526944917f
milovangudelj <milovan.gudelj@gmail.com>
2023-06-17 17:31:55 +0200

    Postponing csp changes

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,6 @@ import Negotiator from "negotiator";
 import { generateSiteMap } from "~lib/sitemap";
 
 import { i18n } from "~/i18n.config";
-import { generateCSP } from "./utils/generateCSP";
 
 function getLocale(request: NextRequest): string | undefined {
 	// Negotiator expects plain object so we need to transform headers
@@ -20,35 +19,16 @@ function getLocale(request: NextRequest): string | undefined {
 }
 
 export const middleware = async (request: NextRequest) => {
-	// generate CSP and nonce
-	const { csp, nonce } = generateCSP();
-
-	// Clone the request headers
-	const requestHeaders = new Headers(request.headers);
-
-	// set nonce request header to read in pages if needed
-	requestHeaders.set("x-nonce", nonce);
-
-	// set CSP header
-	const headerKey = "content-security-policy";
-
-	// Set the CSP header so that `app-render` can read it and generate tags with the nonce
-	requestHeaders.set(headerKey, csp);
-
 	const pathname = request.nextUrl.pathname;
 
 	// Generates sitemap.xml if path is /sitemap.xml
 	if (request.nextUrl.pathname.localeCompare("/sitemap.xml") === 0) {
 		const sitemap = await generateSiteMap();
 
-		const response = new NextResponse(sitemap, {
+		return new NextResponse(sitemap, {
 			status: 200,
-			headers: { "Content-Type": "text/xml", ...requestHeaders },
+			headers: { "Content-Type": "text/xml" },
 		});
-
-		response.headers.set(headerKey, csp);
-
-		return response;
 	}
 
 	// `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
@@ -59,18 +39,7 @@ export const middleware = async (request: NextRequest) => {
 				pathname.startsWith(value) || pathname.localeCompare(value) === 0
 		)
 	) {
-		// create new response
-		const response = NextResponse.next({
-			request: {
-				// New request headers
-				headers: requestHeaders,
-			},
-		});
-
-		// Also set the CSP so that it is outputted to the browser
-		response.headers.set(headerKey, csp);
-
-		return response;
+		return NextResponse.next();
 	}
 
 	// Check if the default locale is in the pathname
@@ -80,22 +49,15 @@ export const middleware = async (request: NextRequest) => {
 	) {
 		// e.g. incoming request is /en/products
 		// The new URL is now /products
-		const response = NextResponse.redirect(
+		return NextResponse.redirect(
 			new URL(
 				pathname.replace(
 					`/${i18n.defaultLocale}`,
 					pathname === `/${i18n.defaultLocale}` ? "/" : ""
 				),
 				request.url
-			),
-			{
-				headers: requestHeaders,
-			}
+			)
 		);
-
-		response.headers.set(headerKey, csp);
-
-		return response;
 	}
 
 	// Check if there is any supported locale in the pathname
@@ -111,42 +73,17 @@ export const middleware = async (request: NextRequest) => {
 		// e.g. incoming request is /products
 		// The new URL is now /en-US/products
 		if (locale === i18n.defaultLocale) {
-			const response = NextResponse.rewrite(
-				new URL(`/${locale}${pathname}`, request.url),
-				{
-					headers: requestHeaders,
-				}
+			return NextResponse.rewrite(
+				new URL(`/${locale}${pathname}`, request.url)
 			);
-
-			response.headers.set(headerKey, csp);
-
-			return response;
 		}
 
-		const response = NextResponse.redirect(
-			new URL(`/${locale}/${pathname}`, request.url),
-			{
-				headers: requestHeaders,
-			}
+		return NextResponse.redirect(
+			new URL(`/${locale}/${pathname}`, request.url)
 		);
-
-		response.headers.set(headerKey, csp);
-
-		return response;
 	}
 
-	// create new response
-	const response = NextResponse.next({
-		request: {
-			// New request headers
-			headers: requestHeaders,
-		},
-	});
-
-	// Also set the CSP so that it is outputted to the browser
-	response.headers.set(headerKey, csp);
-
-	return response;
+	return NextResponse.next();
 };
 
 export const config = {
