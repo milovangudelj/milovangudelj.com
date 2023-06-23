8e414af6ab930f6bde950d17c0c5a485aa022fd3
milovangudelj <milovan.gudelj@gmail.com>
2023-06-23 15:08:41 +0200

    Added `/en` prefix to all canonical urls

--- lib/sitemap.ts
+++ lib/sitemap.ts
@@ -6,35 +6,35 @@ export async function generateSiteMap() {
 	return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
 	<url>
-		<loc>https://www.milovangudelj.com</loc>
-		<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com"/>
+		<loc>https://www.milovangudelj.com/en</loc>
+		<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/en"/>
 		<xhtml:link rel="alternate" hreflang="it" href="https://www.milovangudelj.com/it"/>
 	</url>
 	<url>
-		<loc>https://www.milovangudelj.com/about</loc>
-		<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/about"/>
+		<loc>https://www.milovangudelj.com/en/about</loc>
+		<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/en/about"/>
 		<xhtml:link rel="alternate" hreflang="it" href="https://www.milovangudelj.com/it/about"/>
 	</url>
 	<url>
-		<loc>https://www.milovangudelj.com/work</loc>
-		<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/work"/>
+		<loc>https://www.milovangudelj.com/en/work</loc>
+		<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/en/work"/>
 		<xhtml:link rel="alternate" hreflang="it" href="https://www.milovangudelj.com/it/work"/>
 	</url>
 	<url>
-		<loc>https://www.milovangudelj.com/portfolio</loc>
-		<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/portfolio"/>
+		<loc>https://www.milovangudelj.com/en/portfolio</loc>
+		<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/en/portfolio"/>
 		<xhtml:link rel="alternate" hreflang="it" href="https://www.milovangudelj.com/it/portfolio"/>
 	</url>
 	<url>
-		<loc>https://www.milovangudelj.com/contact</loc>
-		<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/contact"/>
+		<loc>https://www.milovangudelj.com/en/contact</loc>
+		<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/en/contact"/>
 		<xhtml:link rel="alternate" hreflang="it" href="https://www.milovangudelj.com/it/contact"/>
 	</url>
 	${csUrls
 		.map((slug) => {
 			return `<url>
-		<loc>${`https://www.milovangudelj.com/work/${slug}`}</loc>
-		<xhtml:link rel="alternate" hreflang="en" href="${`https://www.milovangudelj.com/work/${slug}`}"/>
+		<loc>${`https://www.milovangudelj.com/en/work/${slug}`}</loc>
+		<xhtml:link rel="alternate" hreflang="en" href="${`https://www.milovangudelj.com/en/work/${slug}`}"/>
 		<xhtml:link rel="alternate" hreflang="it" href="${`https://www.milovangudelj.com/it/work/${slug}`}"/>
 	</url>`;
 		})
