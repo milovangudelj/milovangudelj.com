a166a37529aa143c6a78be20e87e315830bb7955
milovangudelj <milovan.gudelj@gmail.com>
2024-01-29 19:12:14 +0100

    CORS 4

--- apps/website/app/api/setSanityToken/route.ts
+++ apps/website/app/api/setSanityToken/route.ts
@@ -1,5 +1,13 @@
 import { headers } from 'next/headers'
 
+const CORS_HEADERS = {
+  'Access-Control-Allow-Origin':
+    process.env.NODE_ENV === 'production' ? 'https://studio.milovangudelj.com' : '*',
+  'Access-Control-Allow-Credentials': 'true',
+  'Access-Control-Allow-Headers': 'Authorization',
+  'Access-Control-Allow-Methods': 'POST, OPTIONS',
+}
+
 export async function POST() {
   const headersList = headers()
   const authHeader = headersList.get('Authorization')
@@ -12,19 +20,14 @@ export async function POST() {
   const cookie = `loggedInStudio=true; path=/; Secure; SameSite=Strict`
   const resHeaders = new Headers({
     'Set-Cookie': cookie,
+    ...CORS_HEADERS,
   })
 
   return new Response('OK', { status: 200, headers: resHeaders })
 }
 
 export async function OPTIONS() {
-  const resHeaders = new Headers({
-    'Access-Control-Allow-Origin':
-      process.env.NODE_ENV === 'production' ? 'https://studio.milovangudelj.com' : '*',
-    'Access-Control-Allow-Credentials': 'true',
-    'Access-Control-Allow-Headers': 'Authorization',
-    'Access-Control-Allow-Methods': 'POST, OPTIONS',
-  })
+  const resHeaders = new Headers(CORS_HEADERS)
 
   return new Response('OK', { status: 200, headers: resHeaders })
 }
