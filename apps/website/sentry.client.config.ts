f95586985643f244fdf778b5b98f6a7093fe741f
Milovan Gudelj <milovan.gudelj@gmail.com>
2024-06-04 18:47:40 +0200

    Ignore stupid errors

--- apps/website/sentry.client.config.ts
+++ apps/website/sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://d2d7828a98ebdcf5dd156433fad461c1@o1087154.ingest.sentry.io/4506825533161472",
+  dsn: 'https://d2d7828a98ebdcf5dd156433fad461c1@o1087154.ingest.sentry.io/4506825533161472',
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
@@ -27,4 +27,6 @@ Sentry.init({
       blockAllMedia: true,
     }),
   ],
-});
+
+  ignoreErrors: ['wp-login.php'],
+})
