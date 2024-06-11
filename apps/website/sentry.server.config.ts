161b21bfa80c0da17e6c57a99779956972a44a38
Milovan Gudelj <milovan.gudelj@gmail.com>
2024-06-11 14:51:04 +0200

    Still trying to ignore those errors

--- apps/website/sentry.server.config.ts
+++ apps/website/sentry.server.config.ts
@@ -5,11 +5,60 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://d2d7828a98ebdcf5dd156433fad461c1@o1087154.ingest.sentry.io/4506825533161472",
+  dsn: 'https://d2d7828a98ebdcf5dd156433fad461c1@o1087154.ingest.sentry.io/4506825533161472',
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-});
+  ignoreErrors: [
+    // Known Wordpress plugins and php scripts
+    'wp-login.php',
+    'revision.php',
+    '.php',
+    // Random plugins/extensions
+    'top.GLOBALS',
+    // See: http://blog.errorception.com/2012/03/tale-of-unfindable-js-error.html
+    'originalCreateNotification',
+    'canvas.contentDocument',
+    'MyApp_RemoveAllHighlights',
+    'http://tt.epicplay.com',
+    "Can't find variable: ZiteReader",
+    'jigsaw is not defined',
+    'ComboSearch is not defined',
+    'http://loading.retry.widdit.com/',
+    'atomicFindClose',
+    // Facebook borked
+    'fb_xd_fragment',
+    // ISP "optimizing" proxy - `Cache-Control: no-transform` seems to reduce this. (thanks @acdha)
+    // See http://stackoverflow.com/questions/4113268/how-to-stop-javascript-injection-from-vodafone-proxy
+    'bmi_SafeAddOnload',
+    'EBCallBackMessageReceived',
+    // See http://toolbar.conduit.com/Developer/HtmlAndGadget/Methods/JSInjection.aspx
+    'conduitPage',
+    // Generic error code from errors outside the security sandbox
+    // You can delete this if using raven.js > 1.0, which ignores these automatically.
+    'Script error.',
+    // Avast extension error
+    '_avast_submit',
+  ],
+  denyUrls: [
+    // Google Adsense
+    /pagead\/js/i,
+    // Facebook flakiness
+    /graph\.facebook\.com/i,
+    // Facebook blocked
+    /connect\.facebook\.net\/en_US\/all\.js/i,
+    // Woopra flakiness
+    /eatdifferent\.com\.woopra-ns\.com/i,
+    /static\.woopra\.com\/js\/woopra\.js/i,
+    // Chrome extensions
+    /extensions\//i,
+    /^chrome:\/\//i,
+    // Other plugins
+    /127\.0\.0\.1:4001\/isrunning/i, // Cacaoweb
+    /webappstoolbarba\.texthelp\.com\//i,
+    /metrics\.itunes\.apple\.com\.edgesuite\.net\//i,
+  ],
+})
