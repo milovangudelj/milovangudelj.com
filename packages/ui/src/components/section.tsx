9bb5949d5b1454244cf3cfd2bb97111154398be2
Milovan Gudelj <milovan.gudelj@gmail.com>
2024-06-16 02:53:56 +0200

    Added dot grid background

--- packages/ui/src/components/section.tsx
+++ packages/ui/src/components/section.tsx
@@ -3,12 +3,7 @@ import { twMerge } from 'tailwind-merge'
 
 export const Section = ({ children, className = '' }: ComponentProps<'section'>) => {
   return (
-    <section
-      className={twMerge(
-        `ui-border-t ui-border-white/[0.06] ui-px-8 ui-py-32 first-of-type:ui-border-none`,
-        className
-      )}
-    >
+    <section className={twMerge(`ui-border-t ui-border-white/[0.06] ui-px-8 ui-py-32`, className)}>
       {children}
     </section>
   )
