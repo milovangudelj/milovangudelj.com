9efb7b96a9c255835a2e688548ffbb340fe12179
Milovan Gudelj <milovan.gudelj@gmail.com>
2024-06-24 19:50:55 +0200

    Revert last two commits

--- apps/website/components/projects.tsx
+++ apps/website/components/projects.tsx
@@ -1,9 +1,17 @@
 'use client'
 
-import { ReactNode } from 'react'
+import { ComponentProps } from 'react'
 import { useScrollContainer } from 'react-indiana-drag-scroll'
 
-export const Projects = ({ children }: { children: ReactNode }) => {
+import { SlimProjectPayload } from '@repo/sanity/queries'
+
+import { ProjectCard } from './project-card'
+
+interface ProjectsProps extends ComponentProps<'div'> {
+  projects: SlimProjectPayload[]
+}
+
+export const Projects = ({ projects }: ProjectsProps) => {
   const scrollContainer = useScrollContainer({
     mouseScroll: {
       rubberBand: false,
@@ -17,7 +25,11 @@ export const Projects = ({ children }: { children: ReactNode }) => {
         ref={scrollContainer.ref}
         className="scrollbar-hidden -mx-8 flex gap-8 overflow-y-hidden overflow-x-scroll px-8 xl:-mx-[var(--side-width)] xl:gap-16 xl:px-[var(--side-width)]"
       >
-        {children}
+        {projects.map(({ slug, ...props }) => (
+          <li key={slug} className="relative flex-none">
+            <ProjectCard {...props} />
+          </li>
+        ))}
       </ul>
       <span className="absolute -left-8 bottom-0 top-0 w-8 bg-transparent backdrop-blur-sm xl:-left-[var(--side-width)] xl:w-[var(--side-width)]"></span>
       <span className="absolute bottom-0 left-full top-0 w-8 bg-transparent backdrop-blur-sm xl:w-[var(--side-width)]"></span>
