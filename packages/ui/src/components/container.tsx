992d78aab2a8ff2c40f941515e82ae9542e1f677
Milovan Gudelj <milovan.gudelj@gmail.com>
2024-06-24 18:30:31 +0200

    Linting hell

--- packages/ui/src/components/container.tsx
+++ packages/ui/src/components/container.tsx
@@ -1,17 +1,17 @@
-import { ComponentPropsWithoutRef, ElementType, HTMLAttributes, ReactNode } from 'react'
+import { type ComponentPropsWithoutRef, type ElementType, type HTMLAttributes } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 export interface ContainerProps<T extends ElementType> extends HTMLAttributes<T> {
   as?: T
 }
 
-export const Container = <T extends ElementType = 'div'>({
+export function Container<T extends ElementType = 'div'>({
   children,
   as,
   className,
   ...props
-}: ContainerProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ContainerProps<T>>) => {
-  const Component = as || 'div'
+}: ContainerProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ContainerProps<T>>): JSX.Element {
+  const Component = as ?? 'div'
 
   return (
     <Component className={twMerge('ui-mx-auto ui-max-w-7xl ui-space-y-16', className)} {...props}>
