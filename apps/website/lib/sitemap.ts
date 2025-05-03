725733744b0acbf144c9de5aab1369f855f6aced
Milo <49202538+milovangudelj@users.noreply.github.com>
2025-05-03 18:21:56 +0200

    Update sitemap.ts
    New base url

--- apps/website/lib/sitemap.ts
+++ apps/website/lib/sitemap.ts
@@ -1,6 +1,8 @@
 import { client } from '@repo/sanity'
 import { caseStudyPaths } from '@repo/sanity/queries'
 
+const baseUrl = process.env.NEXT_PUBLIC_WEBSITE_URL!;
+
 export async function generateSiteMap() {
   const csUrls = await client.fetch<string[]>(
     caseStudyPaths,
@@ -15,36 +17,36 @@ export async function generateSiteMap() {
   return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
 	<url>
-		<loc>https://www.milovangudelj.com/en</loc>
-		<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/en"/>
-		<xhtml:link rel="alternate" hreflang="it" href="https://www.milovangudelj.com/it"/>
+		<loc>${baseUrl}/en</loc>
+		<xhtml:link rel="alternate" hreflang="en" href="${baseUrl}/en"/>
+		<xhtml:link rel="alternate" hreflang="it" href="${baseUrl}/it"/>
 	</url>
 	<url>
-		<loc>https://www.milovangudelj.com/en/about</loc>
-		<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/en/about"/>
-		<xhtml:link rel="alternate" hreflang="it" href="https://www.milovangudelj.com/it/about"/>
+		<loc>${baseUrl}/en/about</loc>
+		<xhtml:link rel="alternate" hreflang="en" href="${baseUrl}/en/about"/>
+		<xhtml:link rel="alternate" hreflang="it" href="${baseUrl}/it/about"/>
 	</url>
 	<url>
-		<loc>https://www.milovangudelj.com/en/work</loc>
-		<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/en/work"/>
-		<xhtml:link rel="alternate" hreflang="it" href="https://www.milovangudelj.com/it/work"/>
+		<loc>${baseUrl}/en/work</loc>
+		<xhtml:link rel="alternate" hreflang="en" href="${baseUrl}/en/work"/>
+		<xhtml:link rel="alternate" hreflang="it" href="${baseUrl}/it/work"/>
 	</url>
 	<url>
-		<loc>https://www.milovangudelj.com/en/portfolio</loc>
-		<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/en/portfolio"/>
-		<xhtml:link rel="alternate" hreflang="it" href="https://www.milovangudelj.com/it/portfolio"/>
+		<loc>${baseUrl}/en/portfolio</loc>
+		<xhtml:link rel="alternate" hreflang="en" href="${baseUrl}/en/portfolio"/>
+		<xhtml:link rel="alternate" hreflang="it" href="${baseUrl}/it/portfolio"/>
 	</url>
 	<url>
-		<loc>https://www.milovangudelj.com/en/contact</loc>
-		<xhtml:link rel="alternate" hreflang="en" href="https://www.milovangudelj.com/en/contact"/>
-		<xhtml:link rel="alternate" hreflang="it" href="https://www.milovangudelj.com/it/contact"/>
+		<loc>${baseUrl}/en/contact</loc>
+		<xhtml:link rel="alternate" hreflang="en" href="${baseUrl}/en/contact"/>
+		<xhtml:link rel="alternate" hreflang="it" href="${baseUrl}/it/contact"/>
 	</url>
 	${csUrls
     .map((slug) => {
       return `<url>
-		<loc>${`https://www.milovangudelj.com/en/work/${slug}`}</loc>
-		<xhtml:link rel="alternate" hreflang="en" href="${`https://www.milovangudelj.com/en/work/${slug}`}"/>
-		<xhtml:link rel="alternate" hreflang="it" href="${`https://www.milovangudelj.com/it/work/${slug}`}"/>
+		<loc>${baseUrl}/en/work/${slug}</loc>
+		<xhtml:link rel="alternate" hreflang="en" href="${baseUrl}/en/work/${slug}"/>
+		<xhtml:link rel="alternate" hreflang="it" href="${baseUrl}/it/work/${slug}"/>
 	</url>`
     })
     .join('')}
